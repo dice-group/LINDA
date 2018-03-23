bcb5deeadc155b8879a72b9151fdc77907a71df1
Kunal-Jha <kunal94jha@gmail.com>
2018-03-23 23:11:49 +0100

    implemented filters for wrapped array

--- src/main/scala/org/aksw/dice/linda/ews/EWSFinder.scala
+++ src/main/scala/org/aksw/dice/linda/ews/EWSFinder.scala
@@ -1,31 +1,33 @@
 package org.aksw.dice.linda.ews
-import org.apache.spark.sql.{ SparkSession, Encoder, _ }
+import org.apache.spark.sql.{ Row, SparkSession, Encoder, _ }
 import org.slf4j.LoggerFactory
 import org.apache.jena.riot.Lang
 import net.sansa_stack.rdf.spark.io.rdf._
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable
+import org.apache.spark.sql.functions.col
 object EWSFinder {
   private val logger = LoggerFactory.getLogger(this.getClass.getName)
 
   var rules: DataFrame = _
   var subject2OperatorDF: DataFrame = _
   var operator2SubjectDF: DataFrame = _
+  val spark = SparkSession.builder
+    .master("local[*]")
+    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    .appName("LINDA (EWS Finder)")
+    .getOrCreate()
 
   def main(args: Array[String]) = {
     val rulesPath = "Data/rule/*"
     val mapsPath = "Data/Maps/"
-    val spark = SparkSession.builder
-      .master("local[*]")
-      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .appName("LINDA (EWS Finder)")
-      .getOrCreate()
+
     val context = spark.sparkContext
 
     rules = spark.read.parquet(rulesPath)
     subject2OperatorDF = spark.read.parquet(mapsPath + "Subject2OperatorMap/*")
     operator2SubjectDF = spark.read.parquet(mapsPath + "Operator2SubjectMap/*")
-
+    calculateEWS(rules.first())
     spark.stop
   }
 
@@ -33,9 +35,7 @@ object EWSFinder {
     val head = rule.getAs[mutable.WrappedArray[String]](0)
     val body = rule.getAs[mutable.WrappedArray[String]](1)
 
-    for (ele <- head) {
-
-    }
-
+    var headrows = subject2OperatorDF.select(subject2OperatorDF("value")).filter(subject2OperatorDF("key").isin(head: _*))
+    headrows.show()
   }
 }
