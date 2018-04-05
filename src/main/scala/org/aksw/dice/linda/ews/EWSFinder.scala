d7581a76f0edb7d7873a288d01d7bbd33a48dee7
Kunal-Jha <kunal94jha@gmail.com>
2018-04-06 00:56:11 +0200

    Implemented the induction learning as proposed in paper

--- src/main/scala/org/aksw/dice/linda/ews/EWSFinder.scala
+++ src/main/scala/org/aksw/dice/linda/ews/EWSFinder.scala
@@ -1,14 +1,15 @@
 package org.aksw.dice.linda.ews
 import org.apache.spark.sql.{ Row, SparkSession, _ }
 import org.slf4j.LoggerFactory
-
+import scala.collection.mutable
 import net.sansa_stack.rdf.spark.io.rdf._
+import org.apache.spark.sql.functions._
 import org.apache.spark.mllib.linalg.distributed.{ CoordinateMatrix, MatrixEntry }
-object EWSFinder {
+object EWSMining {
   private val logger = LoggerFactory.getLogger(this.getClass.getName)
 
   var rules: DataFrame = _
-  var subject2Id: DataFrame = _
+  var subjectOperatorMap: DataFrame = _
   var operator2Id: DataFrame = _
   val spark = SparkSession.builder
     .master("local[*]")
@@ -17,19 +18,28 @@ object EWSFinder {
     .getOrCreate()
 
   def main(args: Array[String]) = {
-    val rulesPath = "Data/rule/*"
+    val rulesPath = "Data/rules/*"
     val mapsPath = "Data/Maps/"
 
     val context = spark.sparkContext
 
-    rules = spark.read.parquet(rulesPath)
-    subject2Id = spark.read.parquet(mapsPath + "SubjectId/*")
-    operator2Id = spark.read.parquet(mapsPath + "OperatorId/*")
+    var a = spark.read.parquet(rulesPath)
+    subjectOperatorMap = spark.read.parquet(mapsPath + "SubjectOperatorMap/*")
+    rules = a.withColumn("antecedent", explode(col("antecedent")))
 
+    //rules.foreach(r => calculateEWS(r))
+    //calculateEWS(rules.first())
     spark.stop
   }
 
   def calculateEWS(rule: Row) {
 
+    val head = rule.getString(0)
+    val body = rule.getAs[mutable.WrappedArray[String]](1)
+
+    var headFacts = subjectOperatorMap.select(subjectOperatorMap("subject"), subjectOperatorMap("operators")).filter(subjectOperatorMap("operators").isin(head: _*))
+    var bodyFacts = subjectOperatorMap.select(subjectOperatorMap("subject"), subjectOperatorMap("operators")).filter(subjectOperatorMap("operators").isin(body: _*))
+    headFacts.show()
+    bodyFacts.show()
   }
 }
