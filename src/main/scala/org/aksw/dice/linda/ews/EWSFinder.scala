ef0cf21baeba80c5a89cc09b29acb749dadc7e0f
Kunal-Jha <kunal94jha@gmail.com>
2018-04-05 04:22:37 +0200

    added writers for the file

--- src/main/scala/org/aksw/dice/linda/ews/EWSFinder.scala
+++ src/main/scala/org/aksw/dice/linda/ews/EWSFinder.scala
@@ -1,17 +1,15 @@
 package org.aksw.dice.linda.ews
-import org.apache.spark.sql.{ Row, SparkSession, Encoder, _ }
+import org.apache.spark.sql.{ Row, SparkSession, _ }
 import org.slf4j.LoggerFactory
-import org.apache.jena.riot.Lang
+
 import net.sansa_stack.rdf.spark.io.rdf._
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable
-import org.apache.spark.sql.functions.col
+import org.apache.spark.mllib.linalg.distributed.{ CoordinateMatrix, MatrixEntry }
 object EWSFinder {
   private val logger = LoggerFactory.getLogger(this.getClass.getName)
 
   var rules: DataFrame = _
-  var subject2OperatorDF: DataFrame = _
-  var operator2SubjectDF: DataFrame = _
+  var subject2Id: DataFrame = _
+  var operator2Id: DataFrame = _
   val spark = SparkSession.builder
     .master("local[*]")
     .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
@@ -25,20 +23,13 @@ object EWSFinder {
     val context = spark.sparkContext
 
     rules = spark.read.parquet(rulesPath)
-    subject2OperatorDF = spark.read.parquet(mapsPath + "Subject2OperatorMap/*")
-    operator2SubjectDF = spark.read.parquet(mapsPath + "Operator2SubjectMap/*")
-    rules.foreach(rule => calculateEWS(rule))
+    subject2Id = spark.read.parquet(mapsPath + "SubjectId/*")
+    operator2Id = spark.read.parquet(mapsPath + "OperatorId/*")
+
     spark.stop
   }
 
   def calculateEWS(rule: Row) {
-    val head = rule.getAs[mutable.WrappedArray[String]](0)
-    val body = rule.getAs[mutable.WrappedArray[String]](1)
-    var headFacts = operator2SubjectDF.select(operator2SubjectDF("value")) filter (operator2SubjectDF("key").isin(head: _*))
-    var bodyFacts = operator2SubjectDF.select(operator2SubjectDF("value")).filter(operator2SubjectDF("key").isin(body: _*))
-    var NS = headFacts.intersect(bodyFacts)
-    var ABS = bodyFacts.except(headFacts)
-    NS.show()
-    ABS.show()
+
   }
 }
--- src/main/scala/org/askw/dice/linda/miner/mining/RuleMiner.scala
+++ src/main/scala/org/askw/dice/linda/miner/mining/RuleMiner.scala
@@ -7,7 +7,7 @@ import org.apache.jena.riot.Lang
 import net.sansa_stack.rdf.spark.io.rdf._
 import scala.collection.mutable.ListBuffer
 import org.apache.spark.ml.fpm.FPGrowth
-import org.apache.spark.sql.functions;
+import org.apache.spark.sql.functions._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.Encoders
@@ -27,28 +27,29 @@ object RuleMiner {
     val context = spark.sparkContext
     val triplesDF = spark.read.rdf(Lang.NTRIPLES)(input)
     RDF2TransactionMap.readFromDF(triplesDF)
-    val someSchema = List(StructField("resource", StringType, true))
+    val resourceIdSchema = List(StructField("resource", StringType, true))
+    val subjectOperatorSchema = List(StructField("subject", StringType, true), StructField("operators", ArrayType(StringType, true), true))
+    var subjetct2Operator = spark.createDataFrame(RDF2TransactionMap.subject2Operator.map(r => Row(r._1, r._2.map(a => a.toString()))), StructType(subjectOperatorSchema))
     var subjects = RDF2TransactionMap.subject2Operator.map(r => Row(r._1))
-    var operators = RDF2TransactionMap.subject2Operator.map(r => r._2.toList.distinct).flatMap(y => y).distinct().map(a => Row(a.toString()))
-
-    var subject2Id = spark.createDataFrame(subjects, StructType(someSchema)).withColumn("id", functions.row_number().over(Window.orderBy("resource")))
-    var operator2Id = spark.createDataFrame(operators, StructType(someSchema)).withColumn("id", functions.row_number().over(Window.orderBy("resource")))
-    var transactionsRDD = RDF2TransactionMap.subject2Operator.map(r => r._2.toArray.map(a => a.toString()))
-    val fpgrowth = new FPGrowth().setItemsCol("items").setMinSupport(0.02).setMinConfidence(0.6)
+    var subject2Id = spark.createDataFrame(subjects, StructType(resourceIdSchema)).withColumn("id", row_number().over(Window.orderBy("resource")))
+    var transactionsRDD = RDF2TransactionMap.subject2Operator.map(r => r._2.map(a => a.toString()))
+    val fpgrowth = new FPGrowth().setItemsCol("items").setMinSupport(0.02).setMinConfidence(0.5)
     import spark.implicits._
     val model = fpgrowth.fit(transactionsRDD.toDF("items"))
-    model.freqItemsets
-    model.associationRules.show()
+    var frequentOperator2Id = model.freqItemsets.drop("freq").map(r => r.getAs[Seq[String]]("items")).flatMap(a => a).withColumn("id", row_number().over(Window.orderBy("value")))
 
-    /*
+    model.associationRules.write.format("parquet").mode("overwrite").save("Data/rules")
 
+    subject2Id.write.format("parquet").mode("overwrite").save("Data/Maps/SubjectId")
+    frequentOperator2Id.write.format("json").mode("overwrite").save("Data/Maps/OperatorId")
 
+    subjetct2Operator.write.format("parquet").mode("overwrite").save("Data/Maps/SubjectOperatorMap")
 
+    /*Actual operator list
+    var operators = RDF2TransactionMap.subject2Operator.map(r => r._2.toList.distinct).flatMap(y => y).distinct().map(a => Row(a.toString()))
 
-
-    subject2OperatorDF.write.format("parquet").mode("overwrite").save("Data/Maps/Subject2OperatorMap")
-    operator2SubjectDF.write.format("parquet").mode("overwrite").save("Data/Maps/Operator2SubjectMap")
-    model.associationRules.write.format("parquet").mode("overwrite").save("Data/rule")*/
+    var operator2Id = spark.createDataFrame(operators, StructType(someSchema)).withColumn("id", functions.row_number().over(Window.orderBy("resource")))
+    */
     spark.stop
   }
 
