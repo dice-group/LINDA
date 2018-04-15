9c911e23b5aa69a591eb28cf8391910168f1a1bd
Kunal-Jha <kunal94jha@gmail.com>
2018-04-15 19:07:00 +0200

    optimised search process using UDF

--- src/main/scala/org/askw/dice/linda/miner/mining/RuleMiner.scala
+++ src/main/scala/org/askw/dice/linda/miner/mining/RuleMiner.scala
@@ -19,10 +19,11 @@ object RuleMiner {
   val input = "Data/rdf.nt"
   var rules: DataFrame = _
   var subjectOperatorMap: DataFrame = _
+  var transactionsDF: DataFrame = _
   var operator2Id: DataFrame = _
   val resourceIdSchema = List(StructField("resource", StringType, true))
   val subjectOperatorSchema = List(StructField("subject", StringType, true), StructField("operators", ArrayType(StringType, true), true))
-  
+
   def main(args: Array[String]) = {
 
     val spark = SparkSession.builder
@@ -33,37 +34,56 @@ object RuleMiner {
     val context = spark.sparkContext
     val triplesDF = spark.read.rdf(Lang.NTRIPLES)(input)
     RDF2TransactionMap.readFromDF(triplesDF)
-    var subject2Operators = spark.createDataFrame(RDF2TransactionMap.subject2Operator.map(r => Row(r._1, r._2.map(a => a.toString()))), StructType(subjectOperatorSchema))
+    this.subjectOperatorMap = spark.createDataFrame(RDF2TransactionMap.subject2Operator.map(r => Row(r._1, r._2.map(a => a.toString()))), StructType(subjectOperatorSchema))
     var subjects = RDF2TransactionMap.subject2Operator.map(r => Row(r._1))
     var subject2Id = spark.createDataFrame(subjects, StructType(resourceIdSchema)).withColumn("id", row_number().over(Window.orderBy("resource")))
     var transactionsRDD = RDF2TransactionMap.subject2Operator.map(r => r._2.map(a => a.toString()))
     val fpgrowth = new FPGrowth().setItemsCol("items").setMinSupport(0.02).setMinConfidence(0.5)
     import spark.implicits._
-    val model = fpgrowth.fit(transactionsRDD.toDF("items"))
+    this.transactionsDF = transactionsRDD.toDF("items")
+    val model = fpgrowth.fit(transactionsDF)
     var frequentOperator2Id = model.freqItemsets.drop("freq").map(r => r.getAs[Seq[String]]("items")).flatMap(a => a).withColumn("id", row_number().over(Window.orderBy("value")))
-
-    // Writers
     var rules = model.associationRules
-    this.subjectOperatorMap = subject2Operators.withColumn("operator", explode(col("operators"))).drop(col("operators"))
+
     //rules.foreach(r => calculateEWS(r))
-    calculateEWS(rules.first())
+    calculateEWSUsingLearning(rules.first())
     spark.stop
   }
 
-  def calculateEWS(rule: Row) {
+  def calculateEWSUsingSetOperations(rule: Row) {
 
     val head = rule.getSeq(1)
     val body = rule.getSeq(0)
-    val headfacts = subjectOperatorMap.select(col("subject")).where(col("operator").isin(head: _*)).distinct()
-
-    val bodyfacts = subjectOperatorMap.select(col("subject")).where(col("operator").isin(body: _*)).distinct()
+    def containsBody = udf((list: mutable.WrappedArray[String]) => {
+      list.exists(a => body.contains(a))
+    })
+    def containshead = udf((list: mutable.WrappedArray[String]) => {
+      list.exists(a => head.contains(a))
+    })
+    val bodyfacts = subjectOperatorMap.select(col("subject")).where(containsBody(col("operators"))).distinct()
+    val headfacts = subjectOperatorMap.select(col("subject")).where(containshead(col("operators"))).distinct()
 
     val NS = headfacts.intersect(bodyfacts)
     val ABS = bodyfacts.except(headfacts)
 
-    val difference = ABS.except(NS).rdd.map(r=>r.getString(0)).collect()
-    val EWS = subjectOperatorMap.select(col("operator")).where(col("subject").isin(difference:_*)).withColumn("S").distinct()
+    val difference = ABS.except(NS).rdd.map(r => r.getString(0)).collect()
+    val EWS = subjectOperatorMap.select(col("operator")).where(col("subject").isin(difference: _*)).distinct()
+    //TODO add a min support count.
     EWS.show(20, false)
+
+  }
+  def calculateEWSUsingLearning(rule: Row) {
+    val head = rule.getSeq(1)
+    val body = rule.getSeq(0)
+    val freqPatterns = new FPGrowth().setItemsCol("operators").setMinSupport(0.02).setMinConfidence(0.5)
+    println(transactionsDF.schema)
+    def contains = udf((list: mutable.WrappedArray[String]) => {
+      list.exists(a => (body.contains(a) && (!head.contains(a))))
+    })
+    transactionsDF.select(col("items")).where(contains(col("items"))).show(false)
+
+    // Remove all the body operators from the transactions and re run fp growth
+    // negativeTransactions.show()
   }
 
 }
