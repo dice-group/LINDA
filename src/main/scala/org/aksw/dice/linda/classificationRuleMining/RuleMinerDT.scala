57919b1ab2ce42fe60aa5b24056603835d76b9f0
Kunal-Jha <kunal94jha@gmail.com>
2018-05-16 20:24:18 +0200

    Created the writer for libsvm

--- src/main/scala/org/aksw/dice/linda/classificationRuleMining/RuleMinerDT.scala
+++ src/main/scala/org/aksw/dice/linda/classificationRuleMining/RuleMinerDT.scala
@@ -16,6 +16,8 @@ object RuleMinerDT {
   var subject2Id: DataFrame = _
   var operator2Id: DataFrame = _
   val input = "Data/rdf.nt"
+  val SPACE: String = " ";
+  val COLON: String = ":";
   lazy val subjectOperatorSchema = List(StructField("subject", StringType, true), StructField("operators", ArrayType(StringType, true), true))
   lazy val operatorIdSchema = List(StructField("resources", ArrayType(StringType), true))
   lazy val subjectIdSchema = List(StructField("subject", StringType, true))
@@ -31,18 +33,34 @@ object RuleMinerDT {
     val triplesDF = spark.read.rdf(Lang.NTRIPLES)(input)
     RDF2TransactionMap.readFromDF(triplesDF)
     val convertToVector = udf((array: Seq[Long]) => {
-      array.map(a => a.toDouble)
-
+      array.toArray.map(_.toDouble)
     })
     this.subjectOperatorMap = spark.createDataFrame(RDF2TransactionMap.subject2Operator.map(r => Row(r._1, r._2.map(a => a.toString()))), StructType(subjectOperatorSchema)).withColumn("factConf", lit(1.0))
     this.subject2Id = spark.createDataFrame(RDF2TransactionMap.subject2Operator.map(r => Row(r._1)), StructType(subjectIdSchema)).withColumn("subjectIds", monotonically_increasing_id())
     this.operator2Id = spark.createDataFrame(RDF2TransactionMap.subject2Operator.map(r => Row(r._2.map(a => a.toString()))), StructType(operatorIdSchema)).withColumn("operator", explode(col("resources"))).withColumn("operatorIds", monotonically_increasing_id()).drop(col("resources"))
     val libsvmDataset = subjectOperatorMap.withColumn("operator", explode(col("operators")))
-      .join(operator2Id, "operator").groupBy(col("subject"), col("operators")).agg(collect_list(col("operatorIds")).as("x")).join(subject2Id, "subject").drop("operators").drop("factConf").drop("subject").withColumn("operatorsIds", convertToVector(col("x"))).drop("x")
+      .join(operator2Id, "operator").groupBy(col("subject"), col("operators")).agg(collect_list(col("operatorIds")).as("x")).drop("operators").drop("factConf").drop("subject").withColumn("operatorsIds", convertToVector(col("x"))).drop("x")
     val pos = libsvmDataset.select("operatorsIds").where(array_contains(col("operatorsIds"), operator2Id.first().get(1)))
-    val neg = libsvmDataset.select("operatorsIds").where(!array_contains(col("operatorsIds"), operator2Id.first().get(1)))
-    LIBSVMConverter.libsvmwriter(pos, neg)
+    val neg = libsvmDataset.except(pos)
+    libsvmwriter(pos, neg)
     spark.stop
   }
+  def libsvmwriter(acceptedEntities: DataFrame, nonAcceptedEntities: DataFrame) {
+    acceptedEntities.rdd.map(r => {
+      val line: StringBuilder = new StringBuilder()
+      line.append("1").append("\t")
+      r.getSeq[Double](0).foreach(a => {
+        line.append(a + COLON + "1").append(SPACE)
+      })
+      line.toString()
+    }).union(nonAcceptedEntities.rdd.map(r => {
+      val line: StringBuilder = new StringBuilder()
+      line.append("-1").append("\t")
+      r.getSeq[Double](0).foreach(a => {
+        line.append(a + COLON + "1").append(SPACE)
+      })
+      line.toString()
+    })).repartition(1).saveAsTextFile("Data/libsvmfile")
 
+  }
 }
