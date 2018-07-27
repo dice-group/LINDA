a0bcd51d03d6016ce33b870cfec193259dd7fc96
Kunal-Jha <kunal94jha@gmail.com>
2018-07-27 14:01:53 +0200

    Chnaged writing location because of hdfs permission

--- src/main/scala/org/aksw/dice/linda/Utils/LINDAProperties.scala
+++ src/main/scala/org/aksw/dice/linda/Utils/LINDAProperties.scala
@@ -4,28 +4,28 @@ object LINDAProperties {
   /*
    * DATASET CONFIGS
    */
-  // 
+  //
   //TODO: Add Other datasets
   final val DATASET_NAME = "freebase_mtr100_mte100-train"
   final val HDFS_MASTER = "hdfs://172.18.160.17:54310/kunal/"
 
   final val INPUT_DATASET = HDFS_MASTER + DATASET_NAME + ".nt"
 
-  final val INPUT_DATASET_SUBJECT_OPERATOR_MAP = HDFS_MASTER + DATASET_NAME + "/Maps/SubjectOperatorMap/"
-  final val OPERATOR_ID_MAP = HDFS_MASTER + DATASET_NAME + "/Maps/OperatorId/parquet"
-  final val INPUT_DATASET_OPERATOR_SUBJECT_MAP = HDFS_MASTER + DATASET_NAME + "/Maps/OperatorSubjectMap/parquet"
+  final val INPUT_DATASET_SUBJECT_OPERATOR_MAP = "Data/" + DATASET_NAME + "/Maps/SubjectOperatorMap/"
+  final val OPERATOR_ID_MAP = "Data/" + DATASET_NAME + "/Maps/OperatorId/parquet"
+  final val INPUT_DATASET_OPERATOR_SUBJECT_MAP = "Data/" + DATASET_NAME + "/Maps/OperatorSubjectMap/parquet"
 
   /*
    * LINDA CONFIGS
    */
-  final val EWS_RULES_JSON = HDFS_MASTER + "EWS/" + DATASET_NAME + "/Rules/Json"
-  final val EWS_RULES_PARQUET = HDFS_MASTER + "EWS/" + DATASET_NAME + "/Rules/Parquet"
-  final val FACTS_KB_EWS = HDFS_MASTER + "EWS/" + DATASET_NAME + "/New Facts/EWSresult.csv"
-  final val DT_RULES_JSON = HDFS_MASTER + "DT/" + DATASET_NAME + "/Rules/Json"
-  final val DT_RULES_PARQUET = HDFS_MASTER + "DT/" + DATASET_NAME + "/Rules/Parquet"
-  final val FACTS_KB_DT = HDFS_MASTER + "DT/" + DATASET_NAME + "/New Facts/DTresult.csv"
-  final val LIBSVM_DATASET = HDFS_MASTER + "DTAlgo/LIBSVMData/"
-  final val DT_INPUT_DATASET = HDFS_MASTER + "DTAlgo/FinalData/"
+  final val EWS_RULES_JSON = "Data/" + "EWS/" + DATASET_NAME + "/Rules/Json"
+  final val EWS_RULES_PARQUET = "Data/" + "EWS/" + DATASET_NAME + "/Rules/Parquet"
+  final val FACTS_KB_EWS = "Data/" + "EWS/" + DATASET_NAME + "/New Facts/EWSresult.csv"
+  final val DT_RULES_JSON = "Data/" + "DT/" + DATASET_NAME + "/Rules/Json"
+  final val DT_RULES_PARQUET = "Data/" + "DT/" + DATASET_NAME + "/Rules/Parquet"
+  final val FACTS_KB_DT = "Data/" + "DT/" + DATASET_NAME + "/New Facts/DTresult.csv"
+  final val LIBSVM_DATASET = "Data/" + "DTAlgo/LIBSVMData/"
+  final val DT_INPUT_DATASET = "Data/" + "DTAlgo/FinalData/"
 
   /*
    * SPARK CONFIGS
