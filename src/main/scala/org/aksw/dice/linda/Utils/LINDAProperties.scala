f6c5dd1cca4d26f7881c65b77e74b470d5295ce0
Kunal-Jha <kunal94jha@gmail.com>
2018-07-27 12:09:50 +0200

    Change in spark version

--- src/main/scala/org/aksw/dice/linda/Utils/LINDAProperties.scala
+++ src/main/scala/org/aksw/dice/linda/Utils/LINDAProperties.scala
@@ -31,7 +31,7 @@ object LINDAProperties {
    * SPARK CONFIGS
    */
   final val SPARK_SYSTEM_LOCAL = "local[*]"
-  final val SPARK_SYSTEM = "spark://172.18.160.16:3077"
+  final val SPARK_SYSTEM = "spark://172.18.160.16:3090"
   final val SERIALIZER = "spark.serializer"
   final val KYRO_SERIALIZER = "org.apache.spark.serializer.KryoSerializer"
   final val WAREHOUSE = "spark.sql.warehouse.dir"
