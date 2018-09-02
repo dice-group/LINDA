0b2b47485c05b317b802dfa3b3a4630f34dc94db
Kunal jha <kunal94jha@gmail.com>
2018-09-02 13:14:50 +0200

    Update libsvmdatasetCreator.py

--- libsvmdatasetCreator.py
+++ libsvmdatasetCreator.py
@@ -1,4 +1,6 @@
 import collections
+from multiprocessing import Pool
+import multiprocessing
 
 operators = list()
 subject = list()
@@ -6,7 +8,14 @@ subjectOperatorMap = {}
 operator2Id = collections.OrderedDict()
 i = 1
 
-
+def process_files(operator):
+    with open("/Users/Kunal/workspaceThesis/LINDA/Data/freebase_mtr100_mte100-train/LIBSVM/" + str(operator2Id[operator]) +".libsvm", "a") as libfile:
+        for subject, operatorList in subjectOperatorMap.iteritems():
+            if operator in operatorList:
+                libfile.write(construct_line("+1", [operator2Id[item] for item in operatorList if operator2Id[item] !=operator] ))
+            else:
+                libfile.write(construct_line("-1", [operator2Id[item] for item in operatorList]))
+    libfile.close
 
 def construct_line(label, input):
     new_line = [label]
@@ -19,9 +28,11 @@ def construct_line(label, input):
     return new_line
 
 
-with open("/Users/Kunal/workspaceThesis/LINDA/Data/imdb1.nt") as f:
+with open("/Users/Kunal/workspaceThesis/data 2/benchmark/fb15k/freebase_mtr100_mte100-train.nt") as f:
     for line in f:
-        data = line.split("\t")
+        if(" " not in line)
+            continue
+        data = line.split(" ")
         operator = data[1] + "::" + data[2]
         if operator not in operator2Id.iteritems():
             operator2Id[operator] = i
@@ -30,18 +41,13 @@ with open("/Users/Kunal/workspaceThesis/LINDA/Data/imdb1.nt") as f:
         subjectOperatorMap.setdefault(data[0], []).append(operator)
 
 
-with open('/Users/Kunal/workspaceThesis/LINDA/Data/imdb1/operator2Id.csv', 'w') as mapfile:
-    for  operator, id in operator2Id.iteritems():
-        mapfile.write( operator +"," +str(id)+"\n")
+with open('/Users/Kunal/workspaceThesis/LINDA/Data/freebase_mtr100_mte100-train/operator2Id.csv', 'w') as mapfile:
+   for  operator, id in operator2Id.iteritems():
+       mapfile.write( operator +"," +str(id)+"\n")
 mapfile.close
 
-for operator, id in operator2Id.iteritems():
-    with open("/Users/Kunal/workspaceThesis/LINDA/Data/imdb1/LIBSVM/" + str(operator2Id[operator]) +".libsvm", "a") as libfile:
-        for subject, operatorList in subjectOperatorMap.iteritems():
-        if operator in operatorList:
-                libfile.write(construct_line("+1", [operator2Id[item] for item in operatorList if item !=operator] ))
-            else:
-                    libfile.write(construct_line("-1", [operator2Id[item] for item in operatorList]))
 
-        libfile.close
-
+num_cores = multiprocessing.cpu_count()
+print "Cores: ", num_cores
+p = Pool(num_cores)
+p.map(process_files, operator2Id.keys())
